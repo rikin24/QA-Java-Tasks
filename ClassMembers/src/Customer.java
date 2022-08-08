bd235d79abdf7508c5ab477dd7a8249942b5ec53
Rikin <87991358+rikin24@users.noreply.github.com>
2022-08-08 14:36:32 +0100

    Add files via upload

---
+++ ClassMembers/src/Customer.java
@@ -0,0 +1,32 @@
+public class Customer {
+
+    private String firstName;
+    private String surname;
+    private static int numberOfPeople = 0;
+
+    public Customer(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+        numberOfPeople++;
+    }
+
+    public static int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
---
+++ ClassMembers/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Customer.getNumberOfPeople());   // output: 0    No objects instantiated
+
+        Customer firstCustomer = new Customer("Arnold","Rimmer");
+        Customer secondCustomer = new Customer("David","Lister");
+
+        System.out.println(firstCustomer.getFirstName());   // output: Arnold
+        System.out.println(secondCustomer.getFirstName());  // output: David
+
+        System.out.println(firstCustomer.getNumberOfPeople());  // output: 2
+        System.out.println(secondCustomer.getNumberOfPeople()); // output: 2
+        System.out.println(Customer.getNumberOfPeople());       // output: 2
+    }
+}
